f0928e4b79c61a6344e2fcd3cd5f9b27324d65d3
shashanknmp <shashanknmp@gmail.com>
2013-09-11 18:16:37 +0530

    worked on myMoney Tracker

---
+++ src/com/pg/myMoneyTracker/myMoneyMainMenu.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package com.pg.myMoneyTracker;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.pg.R;
+import com.pg.PocketGizmo.PocketGizmoApplication;
+
+/**
+ * @author shashank
+ * 
+ */
+public class myMoneyMainMenu extends ListActivity {
+	private final String TAG = "myMoneyMainMenu";
+	private PocketGizmoApplication pgAppObj;
+
+	/**
+	 * 
+	 */
+	public myMoneyMainMenu() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+
+		getApplicationObject();
+		pgAppObj.logMe(TAG, "onCreate()");
+
+		String strMainMenu[] = getResources().getStringArray(
+				R.array.mymoneymenu);
+
+		ArrayAdapter<String> arrStrMenu = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, strMainMenu);
+		setListAdapter(arrStrMenu);
+	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		// TODO Auto-generated method stub
+		super.onListItemClick(l, v, position, id);
+
+		v.setBackgroundResource(R.drawable.appicon);
+	}
+
+	private void getApplicationObject() {
+		if (pgAppObj == null) {
+			pgAppObj = PocketGizmoApplication.getInstance();
+		}
+	}
+}
