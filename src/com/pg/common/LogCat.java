abe40344a0cb323a67127c01d6f405cf0c4e1621
shashanknmp <shashanknmp@gmail.com>
2013-09-11 18:15:20 +0530

    logging on / off integration

---
+++ src/com/pg/common/LogCat.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.pg.common;
+
+/**
+ * @author shashank
+ *
+ */
+public class LogCat {
+
+	/**
+	 * 
+	 */
+	public LogCat() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
